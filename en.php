1e1c7cbec409799d24d418fb4564c4debb7be2fb
Najxoneq <142342200+Najxoneq@users.noreply.github.com>
2026-02-16 14:51:03 +0100

    Add files via upload

---
+++ en.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    'Home' => 'Home',
+    'Link Converter' => 'Link Converter',
+    'Browse Products' => 'Browse Products',
+    'search_placeholder' => 'Search...',
+
+    'hero_badge' => 'JOIN OUR DISCORD COMMUNITY',
+    'hero_subtitle' => 'FINDS',
+    'hero_description' => 'The best spreadsheet will contain proven and high-quality elements and will be expanded with new items.',
+    'browse_button' => 'Browse products',
+
+    'hero_badge_link' => 'DOŁĄCZ DO NASZEGO DISCORDA',
+    'hero_subtitle_link' => 'FINDS',
+    'hero_description_link' => 'The best link converter on the Polish market, which will be constantly developed.',
+    'browse_button_link' => 'Convert link',
+
+    'card2_title' => 'register link',
+    'card2_desc' => 'Link to the new cheapest and fastest agent',
+
+    'card3_title' => 'link converter',
+    'card3_desc' => 'Convert item links from any agent to your favorite agent',
+
+    'card4_title' => 'Soon...',
+    'card4_desc' => 'Soon...',
+
+    'card2_tag' => 'Agent',
+'card3_tag' => 'Tools',
+'card4_tag' => 'Soon...',
+
+
+];
---
+++ pl.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    'Home' => 'Strona główna',
+    'Link Converter' => 'Konwerter linków',
+    'Browse Products' => 'Przeglądaj produkty',
+    'search_placeholder' => 'Szukaj...',
+
+    'hero_badge' => 'DOŁĄCZ DO NASZEGO DISCORDA',
+    'hero_subtitle' => 'FINDS',
+    'hero_description' => 'Najlepszy arkusz zawiera sprawdzone i wysokiej jakości produkty oraz będzie stale rozwijany.',
+    'browse_button' => 'Przeglądaj produkty',
+
+    'hero_badge_link' => 'DOŁĄCZ DO NASZEGO DISCORDA',
+    'hero_subtitle_link' => 'FINDS',
+    'hero_description_link' => 'Najlepszy konwerter linków na polskim rynku, który będzie stale rozwijany.',
+    'browse_button_link' => 'Przekonwertuj link',
+
+    
+    'card2_title' => 'link rejestracyjny',
+    'card2_desc' => 'Link do najtańszego i najszybszego agenta',
+
+    'card3_title' => 'konwerter linków',
+    'card3_desc' => 'Konwertuj linki produktów między agentami',
+
+    'card4_title' => 'Niedługo...',
+    'card4_desc' => 'Niedługo...',
+
+    'card2_tag' => 'Agent',
+'card3_tag' => 'Narzędzia',
+'card4_tag' => 'Niedługo...',
+
+];
